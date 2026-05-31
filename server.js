d88ed59c3ecaf30156c62a8a63df3720cf9c4d80
mirackayacan33-jpg <mirackayacan33@gmail.com>
2026-05-31 23:55:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get("/", (req,res)=>{
-res.send("SohbetX Server Çalışıyor");
-});
+// 🔥 HTML DOSYASINI GÖSTER
+app.use(express.static("public"));
 
 io.on("connection",(socket)=>{
 console.log("Kullanıcı bağlandı");
